e76249f2fe2e5623ce2e1fcfacff7597556d64ec
Paul Afanasev <pashaafanasev64@gmail.com>
2025-04-08 11:42:48 +0400

    new

--- file_handler.py
+++ file_handler.py
@@ -14,19 +14,19 @@ class FileHandler:
                 target_hash = self.generate_hash(target_file_path)
                 if current_hash == target_hash:
                     return False
-
         existing_versions = database.get_file_versions(src_path)
         next_version = 1
         if existing_versions:
             version_list = [int(v) for v in existing_versions.split(',')]
             next_version = max(version_list) + 1
+        add_date = database.get_setting("add_date") == "True"
+        add_time = database.get_setting("add_time") == "True"
         file_mod_time = os.path.getmtime(src_path)
-        date_str = datetime.fromtimestamp(file_mod_time).strftime("%d.%m.%Y")
-        time_str = datetime.fromtimestamp(file_mod_time).strftime("%H.%M")
-        version_str = f"v{next_version}"
+        date_str = datetime.fromtimestamp(file_mod_time).strftime("%d.%m.%Y") if add_date else ""
+        time_str = datetime.fromtimestamp(file_mod_time).strftime("%H.%M") if add_time else ""
         filename = os.path.basename(src_path)
         name, ext = os.path.splitext(filename)
-        new_name = f"{time_str}_{date_str}_{version_str}_{name}{ext}"
+        new_name = f"v{next_version}_{name}{f'_{time_str}' if add_time else ''}{f'_{date_str}' if add_date else ''}{ext}"
         target_path = os.path.join(target_folder, new_name)
         database.update_file_hash(src_path, current_hash)
         new_versions = f"{existing_versions},{next_version}" if existing_versions else str(next_version)
--- settings.py
+++ settings.py
@@ -11,22 +11,18 @@ class SettingsWindow:
         self.create_widgets()
 
     def create_widgets(self):
-        self.date_format_var = tk.StringVar()
-        self.time_format_var = tk.StringVar()
-        self.version_var = tk.BooleanVar()
+        self.add_date_var = tk.BooleanVar(value=True)
+        self.add_time_var = tk.BooleanVar(value=True)
 
-        self.date_label = tk.Label(self.settings_window, text="Формат даты:")
-        self.date_label.pack(anchor=tk.W)
-        self.date_entry = tk.Entry(self.settings_window, textvariable=self.date_format_var)
-        self.date_entry.pack(fill=tk.X)
+        self.date_check = tk.Checkbutton(
+            self.settings_window, text="Добавить дату", variable=self.add_date_var
+        )
+        self.date_check.pack(anchor=tk.W)
 
-        self.time_label = tk.Label(self.settings_window, text="Формат времени:")
-        self.time_label.pack(anchor=tk.W)
-        self.time_entry = tk.Entry(self.settings_window, textvariable=self.time_format_var)
-        self.time_entry.pack(fill=tk.X)
-
-        self.version_check = tk.Checkbutton(self.settings_window, text="Добавить версию (хеш)", variable=self.version_var)
-        self.version_check.pack(anchor=tk.W)
+        self.time_check = tk.Checkbutton(
+            self.settings_window, text="Добавить время", variable=self.add_time_var
+        )
+        self.time_check.pack(anchor=tk.W)
 
         self.save_button = tk.Button(self.settings_window, text="Сохранить", command=self.save_settings)
         self.save_button.pack()
@@ -34,15 +30,11 @@ class SettingsWindow:
         self.load_settings()
 
     def load_settings(self):
-        self.date_format_var.set(self.database.get_setting("date_format") or "дд.мм.гггг")
-        self.time_format_var.set(self.database.get_setting("time_format") or "чч.мм")
-        stored_version = self.database.get_setting("version")
-        if stored_version is not None:
-            self.version_var.set(stored_version == 'True')
+        self.add_date_var.set(self.database.get_setting("add_date") == "True")
+        self.add_time_var.set(self.database.get_setting("add_time") == "True")
 
     def save_settings(self):
-        self.database.set_setting("date_format", self.date_format_var.get())
-        self.database.set_setting("time_format", self.time_format_var.get())
-        self.database.set_setting("version", str(self.version_var.get()))
+        self.database.set_setting("add_date", str(self.add_date_var.get()))
+        self.database.set_setting("add_time", str(self.add_time_var.get()))
         messagebox.showinfo("Информация", "Настройки сохранены")
         self.settings_window.destroy()
