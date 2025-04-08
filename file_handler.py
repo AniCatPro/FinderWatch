3c77c7cc2712842b265f88b71c2e265c01ae48c9
Paul Afanasev <pashaafanasev64@gmail.com>
2025-04-08 10:21:50 +0400

    new

--- file_handler.py
+++ file_handler.py
@@ -1,24 +1,37 @@
+from datetime import datetime
 import shutil
 import hashlib
 import os
 from database import FileDatabase
 
+
 class FileHandler:
     def copy_file(self, src_path, target_folder):
         file_hash = self.generate_hash(src_path)
+
+        # Создаём новое соединение с БД
         database = FileDatabase()
         existing_hash = database.get_file_hash(src_path)
 
-        filename = os.path.basename(src_path)
-        target_path = os.path.join(target_folder, filename)
+        # Получение текущих настроек
+        date_format = database.get_setting("date_format") or "%d.%m.%Y"
+        time_format = database.get_setting("time_format") or "%H.%M"
+        add_version = database.get_setting("version") == 'True'
 
-        # Проверим, существует ли файл в целевой папке и нужно ли его обновить
+        # Формирование дополнительных частей имени
+        date_str = datetime.now().strftime(date_format)
+        time_str = datetime.now().strftime(time_format)
+        version_str = f"_{file_hash}" if add_version else ""
+
+        # Формирование нового имени файла
+        filename = os.path.basename(src_path)
+        name, ext = os.path.splitext(filename)
+        new_name = f"{date_str}_{time_str}{version_str}_{name}{ext}"
+
+        target_path = os.path.join(target_folder, new_name)
+
         if (existing_hash != file_hash) or (not os.path.exists(target_path)):
             database.update_file_hash(src_path, file_hash)
-
-            if os.path.exists(target_path):
-                target_path = os.path.join(target_folder, f"{file_hash}_{filename}")
-
             shutil.copy2(src_path, target_path)
             return True
 
